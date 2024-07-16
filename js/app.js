ad04fed7ad56b565d7261038fd7fa2ad0141f95a
Sebastian <sebastianmurphy516@gmail.com>
2024-07-16 15:03:42 -0400

    Applet built

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 function getValues(){
     let messageInput = document.getElementById("message").value;
     let alertMessage = document.getElementById("alertMessage");
-    alertMessage.innerText = messageInput;
+    let outputMessage = displayMessage(messageInput, rewind(messageInput));
+    alertMessage.innerHTML = outputMessage;
+
+}
+function rewind(string) {
+    let rewound = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        rewound += string[i];
+    }
+    return rewound;
+}
+function displayMessage(input, output) {
+    document.getElementById("alertMessage").style.opacity = 1;
+    let message = `<div>Your message, <i>${input}</i> reversed is <i>${output}</i></div>`
+    return message;
 }
